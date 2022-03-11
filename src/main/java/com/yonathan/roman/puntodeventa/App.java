07b37c699d773d7345bf330cc1acb7f5ae0d5cbf
Yonathan Román <yonathanr11.12@gmail.com>
2022-03-10 21:27:54 -0600

    first commit

---
+++ src/main/java/com/yonathan/roman/puntodeventa/App.java
@@ -0,0 +1,11 @@
+package com.yonathan.roman.puntodeventa;
+
+/**
+ *
+ * @author Yonathan
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Aplicacion iniciada.");
+    }
+}
