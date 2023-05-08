cd6eb71c375061dbe59a7733d5df514be9fab9e2
Yonathan Román <yonathanr11.12@gmail.com>
2023-05-08 00:24:48 -0600

    avance login

--- src/main/java/com/yonathan/roman/puntodeventa/config/DatabaseApp.java
+++ src/main/java/com/yonathan/roman/puntodeventa/config/DatabaseApp.java
@@ -7,6 +7,7 @@ import io.ebean.datasource.DataSourceConfig;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Set;
+import org.apache.commons.lang3.ObjectUtils;
 import org.reflections.Reflections;
 
 /**
@@ -14,48 +15,53 @@ import org.reflections.Reflections;
  * @author Yonathan
  */
 public class DatabaseApp {
-
+    
     public static Database database;
-
+    
     private static final String HOST = "localhost";
     private static final String PORT = "5432";
     private static final String DB = "punto_de_venta";
     private static final String USER = "postgres";
     private static final String PASS = "root";
-
-    public static void init() {
+    
+    public static Database init() {
         try {
-            System.out.println("Iniciando Database...");
-
-            DataSourceConfig dataSourceConfig = new DataSourceConfig();
-            dataSourceConfig.setUsername(USER);
-            dataSourceConfig.setPassword(PASS);
-            dataSourceConfig.setDriver("org.postgresql.Driver");
-            dataSourceConfig.setUrl("jdbc:postgresql://" + HOST + ":" + PORT + "/" + DB + "?characterEncoding=UTF-8&useSSL=false");
-            dataSourceConfig.setIsolationLevel(2);
-
-            DatabaseConfig config = new DatabaseConfig();
-
-            List<String> packages = new ArrayList<>();
-            packages.add("com.yonathan.roman.puntodeventa.model");
-            config.setPackages(packages);
-
-            String pack = "com.yonathan.roman.puntodeventa.model";
-
-            Reflections reflections = new Reflections(pack);
-            Set<Class<?>> classes = reflections.getSubTypesOf(Object.class);
-
-            config.setClasses(classes);
-            config.addAll(classes);
-
-            config.setDdlGenerate(true);
-            config.setDdlRun(true);
-            config.setDatabasePlatformName("postgres");
-
-            config.setDataSourceConfig(dataSourceConfig);
-
-            database = DatabaseFactory.create(config);
-            System.out.println("Database iniciada.");
+            if (ObjectUtils.isEmpty(database)) {
+                System.out.println("Iniciando Database...");
+                
+                DataSourceConfig dataSourceConfig = new DataSourceConfig();
+                dataSourceConfig.setUsername(USER);
+                dataSourceConfig.setPassword(PASS);
+                dataSourceConfig.setDriver("org.postgresql.Driver");
+                dataSourceConfig.setUrl("jdbc:postgresql://" + HOST + ":" + PORT + "/" + DB + "?characterEncoding=UTF-8&useSSL=false");
+                dataSourceConfig.setIsolationLevel(2);
+                
+                DatabaseConfig config = new DatabaseConfig();
+                
+                List<String> packages = new ArrayList<>();
+                packages.add("com.yonathan.roman.puntodeventa.model");
+                config.setPackages(packages);
+                
+                String pack = "com.yonathan.roman.puntodeventa.model";
+                
+                Reflections reflections = new Reflections(pack);
+                Set<Class<?>> classes = reflections.getSubTypesOf(Object.class);
+                
+                config.setClasses(classes);
+                config.addAll(classes);
+                
+                config.setDdlGenerate(false);
+                config.setDdlRun(true); // Solo poner en TRUE si se decea iniciar de nuevo la DB
+                config.setDatabasePlatformName("postgres");
+                
+                config.setDataSourceConfig(dataSourceConfig);
+                
+                database = DatabaseFactory.create(config);
+                System.out.println("Database iniciada.");
+                return database;
+            } else {
+                return database;
+            }
         } catch (Exception e) {
             System.out.println("Database NO iniciada.");
             throw new RuntimeException("Database: " + e.getMessage());
--- src/main/java/com/yonathan/roman/puntodeventa/controller/LoginController.java
+++ src/main/java/com/yonathan/roman/puntodeventa/controller/LoginController.java
@@ -1,16 +1,30 @@
 package com.yonathan.roman.puntodeventa.controller;
 
+import com.yonathan.roman.puntodeventa.model.Usuario;
+import org.apache.commons.lang3.ObjectUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 /**
  *
  * @author Yonathan
  */
 public class LoginController {
 
+    private static final Logger log = LoggerFactory.getLogger(LoginController.class);
+
     public boolean login(String usuario, String password) {
-        if (usuario.equals("a")) {
-            return true;
-        }else{
-            return false;
+
+        try {
+            Usuario user = new Usuario().db().find(Usuario.class).where().eq("usuario", usuario).findOne();
+            if (ObjectUtils.isNotEmpty(user)) {
+                if (password.equals(user.getPassword())) {
+                    return true;
+                }
+            }
+        } catch (Exception e) {
+            log.error("ERROR: " + e.getMessage());
         }
+        return false;
     }
 }
